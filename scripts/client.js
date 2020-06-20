573832a0507fa458ab6b8e2d1f170d79001a75e9
Paul Joachim <PaulDJoachim@gmail.com>
2020-06-19 22:41:46 -0500

    form input is now added to employee array

--- scripts/client.js
+++ scripts/client.js
@@ -1,6 +1,21 @@
 
 $(readyNow);
 
+let employees = [];
+
 function readyNow() {
-    console.log('JQ')
+    $('#employeeIn').on('click', addEmployee);
+}
+
+function addEmployee() {
+    const employeeObj = {
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        idNumber: $('#idNumber').val(),
+        jobTitle: $('#jobTitle').val(),
+        salary: $('#salary').val()
+    }
+    employees.push(employeeObj);
+    console.log(`added ${employees[employees.length-1].firstName} ${employees[employees.length-1].lastName}`);
+    
 }
